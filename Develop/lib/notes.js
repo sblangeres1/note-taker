b51b2ddd1ca55ca604cffe397d09ffef1ad82e2f
Scottie Blangeres <scottblangeres13@gmail.com>
2022-12-14 18:05:04 -0800

    Created functions to create and delete notes

---
+++ Develop/lib/notes.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+//Create note
+function createNewNote(body, notes) {
+  const note = body;
+  notes.push(note);
+
+  fs.writeFileSync(
+    path.join(__dirname, '../db/db.json'),
+    JSON.stringify({
+      notes
+    }, null, 2)
+  )
+  return note;
+};
+
+//Delete note
+function deleteNote(id, notes) {
+  const removed = notes.splice(id, 1);
+  console.log(notes);
+
+  fs.writeFileSync(
+    path.join(__dirname, '../db/db.json'),
+    JSON.stringify({
+      notes
+    }, null, 2)
+  )
+};
